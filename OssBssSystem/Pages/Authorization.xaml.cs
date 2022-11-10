eaeddaddd8986c3af946bfa08096a9fb6dbb1c04
c1own <c1own@DESKTOP-A2JBAO1>
2022-11-11 00:16:18 +0900

    new funс??wtf??

--- OssBssSystem/Pages/Authorization.xaml.cs
+++ OssBssSystem/Pages/Authorization.xaml.cs
@@ -27,30 +27,77 @@ namespace OssBssSystem.Pages
         {
             InitializeComponent();
         }
-        
+
+        int loginUser = 0;
+        string passwordUser = "0";
+
         public void Validation(object sender, KeyEventArgs e)
+        {
+            if ( loginUser.ToString() != login.Text && passwordUser.ToString() != password.Text)
+            {
+                if (e.Key == Key.Enter)
+                {
+                    if (!(String.IsNullOrEmpty(login.Text)) && !(String.IsNullOrEmpty(password.Text)))
+                    {
+                        loginUser = Convert.ToInt32(login.Text);
+                        passwordUser = password.Text;
+                        if (context.Staffs.Any(x => x.Number == loginUser && x.Password == passwordUser))
+                        {
+                            var rand = new Random();
+                            var s = new StringBuilder();
+                            for (int i = 1; i < 8; i++)
+                            {
+                                s.Append((char)rand.Next('1', 'z'));
+                            }
+                            randomString = s.ToString();
+                            textGeneration.IsEnabled = true;
+                            AutorizationBtn.IsEnabled = true;
+                            RefreshBtn.IsEnabled = true;
+                            MessageBox.Show(randomString);
+
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Заполните поля!");
+                    }
+                }
+            }
+        }
+
+        private void LogIn(object sender, RoutedEventArgs e)
+        {
+            if( !(String.IsNullOrEmpty(textGeneration.Text)) && textGeneration.Text.Equals(randomString))
+            {
+                NavigationService.Navigate(new AccountPage());
+            }
+            else
+            {
+                MessageBox.Show("Введены неверные данные!");
+            }
+        }
+
+        private void RefreshGenerationCode(object sender, RoutedEventArgs e)
+        {
+            var rand = new Random();
+            var s = new StringBuilder();
+            for (int i =1; i < 8; i++)
+            {
+                s.Append((char)rand.Next('1', 'z'));
+            }
+
+            randomString = s.ToString();
+            MessageBox.Show(randomString);
+        }
+
+        private void LoginInput(object sender, KeyEventArgs e)
         {
             if(e.Key == Key.Enter)
             {
-                if( !(String.IsNullOrEmpty(login.Text)) && !(String.IsNullOrEmpty(password.Text)) )
+                var loginText = Convert.ToInt32(login.Text);
+                if (context.Staffs.Any(x => x.Number == loginText))
                 {
-                    int loginUser = Convert.ToInt32(login.Text);
-                    string passwordUser = password.Text;
-                    if(context.Staffs.Any(x=> x.Number == loginUser && x.Password == passwordUser))
-                    {
-                        var rand = new Random();
-                        var s = new StringBuilder();
-                        for(int i= 1 ; i< 8; i++)
-                        {
-                            s.Append((char)rand.Next('A', 'Z'));
-                        }
-                        randomString = s.ToString();
-                        MessageBox.Show(randomString);
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Заполните поля!");
+                    password.IsEnabled = true;
                 }
             }
         }
